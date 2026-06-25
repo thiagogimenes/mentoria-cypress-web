353c511994e6cacefa37caf912601b12ddbe64e0
thiagogimenes <thiagoaticus@gmail.com>
2026-06-24 23:38:19 -0300

    fix cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
-  reporter:  'mochawesome',
+  reporter:  'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
